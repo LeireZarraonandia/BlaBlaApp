7360e9d1ff018005406ff1a43046c065390752de
Ander Martinez <andermarlop@gmail.com>
2015-12-18 13:07:12 +0100

    TraductorActivity

--- AndroidApp/BlaBlaTrip/app/src/main/java/es/tta/blablatrip/InicioActivity.java
+++ AndroidApp/BlaBlaTrip/app/src/main/java/es/tta/blablatrip/InicioActivity.java
@@ -1,9 +1,9 @@
 package es.tta.blablatrip;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.view.View;
 
 
 public class InicioActivity extends AppCompatActivity
@@ -13,5 +13,11 @@ public class InicioActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_inicio);
+    }
+
+    public void traductor (View view)
+    {
+        Intent intent = new Intent(this, TraductorActivity.class);
+        startActivity(intent);
     }
 }
