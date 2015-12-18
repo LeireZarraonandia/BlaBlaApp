cf30d38ee9c9a0669f6de69bed06d082587f4add
Ander Martinez <andermarlop@gmail.com>
2015-12-18 09:36:37 +0100

    InicioActivity

---
+++ AndroidApp/BlaBlaTrip/app/src/main/java/es/tta/blablatrip/InicioActivity.java
@@ -0,0 +1,17 @@
+package es.tta.blablatrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+
+public class InicioActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_inicio);
+    }
+}
