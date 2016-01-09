0b946fae7d274dd4fc268f19a2991dba71bbaf2c
Leire <zarroza8@hotmail.com>
2016-01-09 21:38:33 +0100

    getTest a medias

--- AndroidApp/BlaBlaTrip/app/src/main/java/es/tta/blablatrip/presentation/Data.java
+++ AndroidApp/BlaBlaTrip/app/src/main/java/es/tta/blablatrip/presentation/Data.java
@@ -1,7 +1,14 @@
 package es.tta.blablatrip.presentation;
 
+import es.tta.blablatrip.comms.RestClient;
+import es.tta.blablatrip.model.Test;
+
 /**
  * Created by leire on 9/01/16.
  */
 public class Data {
+
+    public Test getTest (String pais){
+        
+    }
 }
